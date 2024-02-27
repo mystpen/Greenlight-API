ce45e723666812aaca5be36a745237fb3666ec15
dokshiye <okshieva@gmail.com>
2024-02-27 16:00:40 +0600

    feat: add new movies endpoints(POST, GET)

---
+++ cmd/api/movies.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// When httprouter is parsing a request, any interpolated URL parameters will be
+	// stored in the request context. We can use the ParamsFromContext() function to
+	// retrieve a slice containing these parameter names and values.
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
