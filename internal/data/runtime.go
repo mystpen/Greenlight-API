3600779364f7c844a11c28041a1a97af70886528
dokshiye <okshieva@gmail.com>
2024-03-14 20:48:03 +0600

    feat: add validator

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -22,23 +22,17 @@ func (r Runtime) MarshalJSON() ([]byte, error) { // for Marshaler interface
 }
 
 func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
-	// We expect that the incoming JSON value will be a string in the format
-	// "<runtime> mins", and the first thing we need to do is remove the surrounding
-	// double-quotes from this string. If we can't unquote it, then we return the
-	// ErrInvalidRuntimeFormat error.
+
 	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
 	if err != nil {
 		return ErrInvalidRuntimeFormat
 	}
-	// Split the string to isolate the part containing the number.
+
 	parts := strings.Split(unquotedJSONValue, " ")
-	// Sanity check the parts of the string to make sure it was in the expected format.
-	// If it isn't, we return the ErrInvalidRuntimeFormat error again.
 	if len(parts) != 2 || parts[1] != "mins" {
 		return ErrInvalidRuntimeFormat
 	}
-	// Otherwise, parse the string containing the number into an int32. Again, if this
-	// fails return the ErrInvalidRuntimeFormat error.
+
 	i, err := strconv.ParseInt(parts[0], 10, 32)
 	if err != nil {
 		return ErrInvalidRuntimeFormat
